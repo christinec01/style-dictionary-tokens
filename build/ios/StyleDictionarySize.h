e085d0fd869fe91d5bec7d4b99254eb3290779b2
Christine Casebolt <christine.casebolt@affirm.com>
2021-01-06 13:50:47 -0800

    initial commit

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+// Do not edit directly
+// Generated on Wed, 06 Jan 2021 19:49:35 GMT
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
