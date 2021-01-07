f1ee17a67a556169d7bc80f3da55c5fc56000042
Christine Casebolt <christine.casebolt@affirm.com>
2021-01-07 11:13:57 -0800

    adds colors and modifies config to generate css

--- build/ios-swift/StyleDictionary.swift
+++ build/ios-swift/StyleDictionary.swift
@@ -3,21 +3,35 @@
 // StyleDictionary.swift
 //
 // Do not edit directly
-// Generated on Wed, 06 Jan 2021 19:49:35 GMT
+// Generated on Thu, 07 Jan 2021 19:11:52 GMT
 //
 
 
 import UIKit
 
 public class StyleDictionary {
-    public static let colorBaseGrayDark = UIColor(red: 0.067, green: 0.067, blue: 0.067, alpha:1)
-    public static let colorBaseGrayLight = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha:1)
-    public static let colorBaseGrayMedium = UIColor(red: 0.600, green: 0.600, blue: 0.600, alpha:1)
-    public static let colorBaseGreen = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha:1)
-    public static let colorBaseRed = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha:1)
-    public static let colorFontBase = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha:1)
-    public static let colorFontSecondary = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha:1)
-    public static let colorFontTertiary = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha:1)
+    public static let colorBaseBlack = UIColor(red: 0.063, green: 0.094, blue: 0.125, alpha:1)
+    public static let colorBaseErrorAccent = UIColor(red: 1.000, green: 0.427, blue: 0.314, alpha:1)
+    public static let colorBaseErrorDark = UIColor(red: 0.576, green: 0.204, blue: 0.184, alpha:1)
+    public static let colorBaseErrorDim = UIColor(red: 0.827, green: 0.263, blue: 0.200, alpha:1)
+    public static let colorBaseErrorLight = UIColor(red: 0.969, green: 0.890, blue: 0.886, alpha:1)
+    public static let colorBaseErrorMidtone = UIColor(red: 0.996, green: 0.667, blue: 0.592, alpha:1)
+    public static let colorBasePrimaryAccent = UIColor(red: 0.290, green: 0.290, blue: 0.957, alpha:1)
+    public static let colorBasePrimaryDark = UIColor(red: 0.129, green: 0.129, blue: 0.537, alpha:1)
+    public static let colorBasePrimaryDim = UIColor(red: 0.184, green: 0.184, blue: 0.757, alpha:1)
+    public static let colorBasePrimaryLight = UIColor(red: 0.902, green: 0.902, blue: 0.988, alpha:1)
+    public static let colorBasePrimaryMidtone = UIColor(red: 0.659, green: 0.663, blue: 0.988, alpha:1)
+    public static let colorBaseSuccessAccent = UIColor(red: 0.165, green: 0.737, blue: 0.478, alpha:1)
+    public static let colorBaseSuccessDark = UIColor(red: 0.063, green: 0.329, blue: 0.196, alpha:1)
+    public static let colorBaseSuccessDim = UIColor(red: 0.039, green: 0.537, blue: 0.298, alpha:1)
+    public static let colorBaseSuccessLight = UIColor(red: 0.910, green: 0.976, blue: 0.937, alpha:1)
+    public static let colorBaseSuccessMidtone = UIColor(red: 0.592, green: 0.910, blue: 0.722, alpha:1)
+    public static let colorBaseWarningAccent = UIColor(red: 1.000, green: 0.792, blue: 0.380, alpha:1)
+    public static let colorBaseWarningDark = UIColor(red: 0.859, green: 0.392, blue: 0.078, alpha:1)
+    public static let colorBaseWarningDim = UIColor(red: 0.910, green: 0.549, blue: 0.192, alpha:1)
+    public static let colorBaseWarningLight = UIColor(red: 0.988, green: 0.976, blue: 0.910, alpha:1)
+    public static let colorBaseWarningMidtone = UIColor(red: 1.000, green: 0.914, blue: 0.561, alpha:1)
+    public static let colorBaseWhite = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha:1)
     public static let sizeFontBase = CGFloat(16.00)
     public static let sizeFontLarge = CGFloat(32.00)
     public static let sizeFontMedium = CGFloat(16.00)
--- build/ios/StyleDictionaryColor.h
+++ build/ios/StyleDictionaryColor.h
@@ -3,21 +3,35 @@
 // StyleDictionaryColor.h
 //
 // Do not edit directly
-// Generated on Wed, 06 Jan 2021 19:49:35 GMT
+// Generated on Thu, 07 Jan 2021 19:11:52 GMT
 //
 
 #import <UIKit/UIKit.h>
 
 
 typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
-ColorBaseGrayLight,
-ColorBaseGrayMedium,
-ColorBaseGrayDark,
-ColorBaseRed,
-ColorBaseGreen,
-ColorFontBase,
-ColorFontSecondary,
-ColorFontTertiary
+ColorBasePrimaryLight,
+ColorBasePrimaryMidtone,
+ColorBasePrimaryAccent,
+ColorBasePrimaryDim,
+ColorBasePrimaryDark,
+ColorBaseWarningLight,
+ColorBaseWarningMidtone,
+ColorBaseWarningAccent,
+ColorBaseWarningDim,
+ColorBaseWarningDark,
+ColorBaseErrorLight,
+ColorBaseErrorMidtone,
+ColorBaseErrorAccent,
+ColorBaseErrorDim,
+ColorBaseErrorDark,
+ColorBaseSuccessLight,
+ColorBaseSuccessMidtone,
+ColorBaseSuccessAccent,
+ColorBaseSuccessDim,
+ColorBaseSuccessDark,
+ColorBaseBlack,
+ColorBaseWhite
 };
 
 @interface StyleDictionaryColor : NSObject
